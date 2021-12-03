e3f9c37cfac5f6883c68be08ee195b914a2ddf5c
i-nishi <nishi21041998@gmail.com>
2021-12-04 02:12:07 +0530

    modified

--- src/pattern/Pattern13.java
+++ src/pattern/Pattern13.java
@@ -6,7 +6,7 @@ public class Pattern13 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        for (int i = 0; i <= n; i++) {
+        for (int i = 0; i < n; i++) {
             int icj = 1;
             for (int j = 0; j <= i; j++) {
                 System.out.print(icj + "\t");
