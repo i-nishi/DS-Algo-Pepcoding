2da6456244301ab30bc2fe21a4558c0f21bfca4b
i-nishi <nishi21041998@gmail.com>
2021-12-04 04:38:50 +0530

    inverted

---
+++ src/pattern/Pattern19.java
@@ -0,0 +1,11 @@
+package pattern;
+
+import java.util.Scanner;
+
+public class Pattern19 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+    }
+}
--- src/pattern/PatternBonus.java
+++ src/pattern/PatternBonus.java
@@ -8,22 +8,38 @@ public class PatternBonus {
         int n = scn.nextInt();
         int sp = 0;
         int st = n;
+        int rst = -1;
         for (int i = 1; i <= n; i++) {
-            for (int k = 1; k <= sp; k++) {
-                System.out.print("\t");
-            }
-            for (int j = st; j >= 1; j--) {
-                System.out.print("*\t");
-            }
             if (i <= n / 2) {
+                for (int k = 1; k <= sp; k++) {
+                    System.out.print("\t");
+                }
+                for (int j = st; j >= 1; j--) {
+                    System.out.print("*\t");
+                }
+                System.out.println();
                 st -= 2;
                 sp++;
-            }else{
+            } else if (i == n) {
+                for (int k = 1; k <= n; k++) {
+                    System.out.print("*\t");
+                }
+            } else {
+                for (int k = 1; k <= sp; k++) {
+                    System.out.print("\t");
+                }
+                System.out.print("*\t");
+                if (i > (n/2)+1) {
+                    for (int m = 1; m <= rst; m++) {
+                        System.out.print("\t");
+                    }
+                    System.out.print("*\t");
+                }
+                rst+=2;
                 st += 2;
                 sp--;
+                System.out.println();
             }
-            System.out.println();
         }
     }
 }
-
