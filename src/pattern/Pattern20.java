0287ce4d19c6880e39d7aba90dd1848031c97298
i-nishi <nishi21041998@gmail.com>
2021-12-06 02:45:52 +0530

    pattern20

--- src/pattern/Pattern20.java
+++ src/pattern/Pattern20.java
@@ -7,19 +7,14 @@ public class Pattern20 {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                if (j == 1 || j == i && j <= (n/2)+1) {
+            for (int j = 1; j <= n; j++) {
+                if (j == 1 || j == n) {
                     System.out.print("*\t");
-                }else{
+                } else if (i > n / 2 && (i == j || i + j == n + 1)) {
+                    System.out.print("*\t");
+                } else {
                     System.out.print("\t");
                 }
-            }
-            for (int k = n; k >= i; k--) {
-//                if (k == 1) {
-                    System.out.print("0\t");
-//                }else{
-//                    System.out.print("\t");
-//                }
             }
             System.out.println();
         }
