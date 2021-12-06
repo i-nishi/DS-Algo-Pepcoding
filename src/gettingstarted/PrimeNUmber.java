961ae64f083a747dfa369c23a92c4385e8bf042f
i-nishi <nishi21041998@gmail.com>
2021-12-07 01:02:03 +0530

    prime

--- src/gettingstarted/PrimeNUmber.java
+++ src/gettingstarted/PrimeNUmber.java
@@ -1,7 +1,24 @@
 package gettingstarted;
 
+import java.util.Scanner;
+
 public class PrimeNUmber {
     public static void main(String[] args) {
-
+        Scanner scn = new Scanner(System.in);
+        int t = scn.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = scn.nextInt();
+            int count = 0;
+            for (int j = 2; j < n; j++) {
+                if (n % j == 0) {
+                    count++;
+                }
+            }
+            if (count == 0) {
+                System.out.println("prime");
+            }else{
+                System.out.println("not prime");
+            }
+        }
     }
 }
