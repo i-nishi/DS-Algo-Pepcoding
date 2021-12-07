6b9ea8048219363a49b97f980290c0e5a7accae3
i-nishi <nishi21041998@gmail.com>
2021-12-08 00:58:55 +0530

    pytha

--- src/gettingstarted/PythagoreanTriplets.java
+++ src/gettingstarted/PythagoreanTriplets.java
@@ -8,6 +8,20 @@ public class PythagoreanTriplets {
         int n1 = scn.nextInt();
         int n2 = scn.nextInt();
         int n3 = scn.nextInt();
-        int a = n1;
+        int max = n1;
+        if (n2 >= max) {
+            max = n2;
+        }
+        if (n3 >= max) {
+            max = n3;
+        }
+        if (max == n1) {
+            System.out.println((n2 * n2) + (n3 * n3) == (n1 * n1));
+        }
+        else if (max == n2) {
+            System.out.println((n1 * n1) + (n3 * n3) == (n2 * n2));
+        }else{
+            System.out.println((n1 * n1) + (n2 * n2) == (n3 * n3));
+        }
     }
 }
